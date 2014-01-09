49128e57c421efaab28f503a925778f298051e7c
Fun-User <Gerry Sommerville@GerrySommervill>
2014-01-08 20:00:07 -0500

    Updated Trace.groovy, some intended functionality is still not present

--- src/plutoDNA_simulator/Trace.groovy
+++ src/plutoDNA_simulator/Trace.groovy
@@ -4,37 +4,68 @@ import java.util.Date
 
 class Trace {
 
-
-	def static final LOG_LEVEL = "info"
+	
+	def static LEVEL = LOG_LEVEL.ERROR
+	
+	static enum LOG_LEVEL {
+		ERROR, WARNING, INFO
+	}
+	
+	def static setLogLevel(level){
 		
-	def static info(className, string, threadID = ""){
-		if(LOG_LEVEL =~ /info/){
+		
+		
+		
+		if(!(level instanceof LOG_LEVEL) && !(level instanceof Integer)){
+			throw new Exception()
+		}
+		
+		if (level==LOG_LEVEL.ERROR || (int)level == LOG_LEVEL.ERROR.value()){
+			LOG_LEVEL.ERROR
+		}
+		else if (level==LOG_LEVEL.WARNING || (int)level == LOG_LEVEL.WARNING.value()){
+			LOG_LEVEL.WARNING
+		}
+		else if (level==LOG_LEVEL.INFO || (int)level == LOG_LEVEL.INFO.value()){
+			LOG_LEVEL.INFO
+		}
+		else{
+			throw new Exception()
+		}
+		
+		
+	}
+		
+	def static info(className = "trace", string, threadID = ""){
+		if (LEVEL >= LOG_LEVEL.INFO){
 			string = threadID +"INFO: " + string
 			log(className, string)
 		}
 	}
 	
-	def static warning(className, string, threadID = ""){
+	def static warning(className = "trace", string, threadID = ""){
 		
-		if(LOG_LEVEL =~ /warning/){
+		if(LEVEL <= LOG_LEVEL.WARNING){
 			string = threadID +"WARNING: " + string 
 			log(className, string)
 		}
 	}
 	
 	
-	def static error(className, string, threadID = ""){
+	def static error(className = "trace", string, threadID = ""){
 		
-		if(LOG_LEVEL =~ /error/){
+		
+		if(LEVEL <= LOG_LEVEL.ERROR){
 			string = threadID +"ERROR: " + string
 			log(className, string)
 		}
 	}
 	
-	def static log(className, string){
+	def static log(className, threadID = "", string){
 		
-		def timeStamp = Date.getDateTimeString()
-		def f = new File("className" + timeStamp).withWriter { out ->
+		//def date = new Date()
+		//String timeStamp = date.getDateTimeString()
+		def f = new File(className + ".log").withWriter { out ->
 			out.writeLine(string)
 		}
 		
